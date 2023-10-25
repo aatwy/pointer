0437b5af1defae12e33b65cbc242551005973c00
aatwy <aatwy@foodlogiq.com>
2023-10-24 20:17:55 -0400

    Initial comoit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayersComponent } from './players/players.component';
+import { SessionComponent } from './session/session.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SessionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerComponent } from './player/player.component';
+import { HeaderComponent } from './header/header.component';
+import { SessionComponent } from './session/session.component';
+import { HistoryComponent } from './history/history.component';
+import { StoryComponent } from './story/story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent,
+    PlayerComponent,
+    HeaderComponent,
+    SessionComponent,
+    HistoryComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule,
