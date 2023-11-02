d0d7d1f0dd3d95955ac9454ac869b832b1d003f8
aatwy <aatwy@foodlogiq.com>
2023-11-01 21:03:32 -0400

    UI complete

--- src/app/players/player/player.component.ts
+++ src/app/players/player/player.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-player',
+  selector: 'tr[app-player]',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
